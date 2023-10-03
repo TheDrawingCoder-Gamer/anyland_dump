fbfe2369aef10bec3abb952681aa5b1ce23df12f
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2023-10-03 15:39:23 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using System.IO;
+using HarmonyLib;
+using UnityEngine;
+namespace DumpInv {
+	[BepInPlugin("net.bulbyvr.anyland.DumpInv", "Dump Inventory", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		void Awake() {
+			Harmony.CreateAndPatchAll(typeof(Patches));
+		}
+
+		void Update() {
+			
+		}
+	}
+	public class Patches {
+		[HarmonyPatch(typeof(InventoryDialog), "Open")]
+		[HarmonyPostfix]
+		static void OnInvOpen(InventoryDialog __instance) 
+		{	
+			loadInv(__instance);	
+		}
+		static void loadInv(InventoryDialog __instance) {
+			string path = Paths.BepInExRootPath + "/invdump";
+			if (Directory.Exists(path))
+				return;
+			Directory.CreateDirectory(path);
+			ServerManager manager = Managers.serverManager;
+			for (int i = 0; i < 100; i++)
+			{
+				int cur = i;
+				__instance.StartCoroutine(manager.LoadInventory(i, delegate(LoadInventory_Response response) {
+							Debug.Log($"Page {cur}");
+							Directory.CreateDirectory(path + $"/page{cur}");
+							foreach (InventoryItemData item in response.inventoryItems.inventoryItems)
+							{
+								string id = item.thingId;
+								__instance.StartCoroutine(manager.GetThingDefinition(id, delegate(GetThingDefinition_Response response1) 
+										{
+											File.WriteAllText(path + $"/page{cur}/{id}.json", response1.thingDefinitionJSON);
+										}));
+							}
+						}));
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Anyland Exporter
+
+Exports your inventory when you open it in anyland.
+
+Only dumps when there is no `BepInEx/invdump` directory
+
+
