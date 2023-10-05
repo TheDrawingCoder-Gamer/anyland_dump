aab7773448d51c634bcfce5ab9e1e910794a94b7
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2023-10-05 13:33:21 -0400

    Fetch subthings and included items

--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using HarmonyLib;
 using UnityEngine;
+using System.Collections.Generic;
+using SimpleJSON;
+using System.Linq;
 namespace DumpInv {
 	[BepInPlugin("net.bulbyvr.anyland.DumpInv", "Dump Inventory", "1.0.0")]
 	public class Plugin : BaseUnityPlugin
@@ -36,13 +39,50 @@ namespace DumpInv {
 							foreach (InventoryItemData item in response.inventoryItems.inventoryItems)
 							{
 								string id = item.thingId;
-								__instance.StartCoroutine(manager.GetThingDefinition(id, delegate(GetThingDefinition_Response response1) 
-										{
-											File.WriteAllText(path + $"/page{cur}/{id}.json", response1.thingDefinitionJSON);
-										}));
+								dumpInator(__instance, id, $"{path}/page{cur}");
 							}
 						}));
 			}
+		}
+		static void dumpInator(InventoryDialog __instance, string id, string root) {
+			ServerManager manager = Managers.serverManager;
+			__instance.StartCoroutine(manager.GetThingDefinition(id, delegate(GetThingDefinition_Response response)
+						{
+							File.WriteAllText($"{root}/{id}.json", response.thingDefinitionJSON);
+							JSONObject res = JSON.Parse(response.thingDefinitionJSON).AsObject;
+							if (Enumerable.Contains(res.Keys, "inc"))
+							{
+								
+								Directory.CreateDirectory($"{root}/{id}/inc");
+								foreach (JSONNode item in res["inc"].AsArray) 
+								{
+									string incId = item.AsArray[1].Value;
+									dumpInator(__instance, incId, $"{root}/{id}/inc");
+								}
+							}
+							foreach (JSONNode part in res["p"].AsArray) 
+							{
+								
+								if (Enumerable.Contains(part.Keys, "i")) 
+								{
+									Directory.CreateDirectory($"{root}/{id}/incsub");
+									foreach (JSONNode item in part["i"].AsArray) 
+									{
+										string subId = item.AsObject["t"].Value;
+										dumpInator(__instance, subId, $"{root}/{id}/incsub");
+									}
+								}
+								if (Enumerable.Contains(part.Keys, "su"))
+								{
+									Directory.CreateDirectory($"{root}/{id}/psub");
+									foreach (JSONNode item in part["su"].AsArray)
+									{
+										string subId = item.AsObject["t"].Value;
+										dumpInator(__instance, subId, $"{root}/{id}/psub");
+									}
+								}
+							}
+						}));
 		}
 	}
 }
